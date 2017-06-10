32b66ea1228bdd11d4f50c985f4513852cabf427
Michael L Perry <michael@qedcode.com>
2017-06-10 11:02:59 -0500

    Added mathematician name to genome.

--- Mathematicians.DAL/Genome.cs
+++ Mathematicians.DAL/Genome.cs
@@ -9,28 +9,53 @@ namespace Mathematicians.API
         {
             var dbo = db.UseSchema("dbo");
 
-            CreateMathematician(db, dbo);
+            CreateMathematician(dbo);
         }
 
-        private void CreateMathematician(DatabaseSpecification db, SchemaSpecification dbo)
+        private void CreateMathematician(SchemaSpecification dbo)
         {
             var table = dbo.CreateTable("Mathematician");
 
             var id = table.CreateIdentityColumn("MathematicianId");
             var unique = table.CreateGuidColumn("Unique");
 
-            table.CreatePrimaryKey(id);
+            var pk = table.CreatePrimaryKey(id);
+            table.CreateUniqueIndex(unique);
 
-            var generateIds = db
-                .After(table, unique)
-                .Execute(@"
-                    UPDATE dbo.Mathematician
-                    SET [Unique] = NEWID()
-                    WHERE [Unique] = '00000000-0000-0000-0000-000000000000'");
+            CreateMathematicianName(dbo, pk);
+        }
 
-            table
-                .After(generateIds)
-                .CreateUniqueIndex(unique);
+        private void CreateMathematicianName(SchemaSpecification dbo, PrimaryKeySpecification mathematicianPk)
+        {
+            var table = dbo.CreateTable("MathematicianName");
+
+            var id = table.CreateIdentityColumn("MathematicianNameId");
+            var mathematicianId = table.CreateIntColumn("MathematicianId");
+            var firstName = table.CreateStringColumn("FirstName", 50);
+            var lastName = table.CreateStringColumn("LastName", 50);
+
+            var index = table.CreateIndex(mathematicianId);
+            var fk = index.CreateForeignKey(mathematicianPk);
+
+            var pk = table.CreatePrimaryKey(id);
+
+            CreateMathematicianNamePrior(dbo, pk);
+        }
+
+        private static void CreateMathematicianNamePrior(SchemaSpecification dbo, PrimaryKeySpecification mathematicianPk)
+        {
+            var table = dbo.CreateTable("MathematicianNamePrior");
+
+            var currentId = table.CreateIntColumn("MathematicianNameId");
+            var priorId = table.CreateIntColumn("PriorMathematicianNameId");
+
+            var currentIndex = table.CreateIndex(currentId);
+            var currentFk = currentIndex.CreateForeignKey(mathematicianPk);
+
+            var priorIndex = table.CreateIndex(priorId);
+            var priorFk = priorIndex.CreateForeignKey(mathematicianPk);
+
+            var pk = table.CreatePrimaryKey(currentId, priorId);
         }
     }
 }
